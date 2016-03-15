0fb65d3e179f4c78612c355df4f63c3efe173ea7
dmt5dh <dmt5dh@virginia.edu>
2016-03-15 01:01:20 -0400

    Logged final game

--- app/src/main/java/arashincleric/com/spinner/FinalGameActivity.java
+++ app/src/main/java/arashincleric/com/spinner/FinalGameActivity.java
@@ -7,17 +7,18 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class FinalGameActivity extends AbstractTaskActivity implements FinalGameFragment.OnTaskFragmentInteractionListener {
 
     ArrayList<Wheel> wheelList;
-    String userID;
     int gameNum;
 
     Fragment mContent;
@@ -29,7 +30,7 @@ public class FinalGameActivity extends AbstractTaskActivity implements FinalGame
 
         Intent intent = getIntent();
         wheelList = intent.getParcelableArrayListExtra("WHEELLIST");
-        userID = intent.getStringExtra("USERNAME");
+        super.userID = intent.getStringExtra("USERNAME");
         gameNum = intent.getIntExtra("GAMENUM", 0);
 
         TextView gameNumText = (TextView)findViewById(R.id.gameNumberText);
@@ -62,8 +63,13 @@ public class FinalGameActivity extends AbstractTaskActivity implements FinalGame
 
     @Override
     public void logEventTask(String action, String result, String outcome){
-        //TODO: log the score for the player here
-        //TODO: freeze everything on final game fragment
+        //LOG: log the score for the player here
+        try{
+            logEvent(Calendar.getInstance(), "Payment", "Game " + gameNum, result, outcome);
+        }
+        catch (Exception e){
+            Log.e("ERROR", "Error logging payment data");
+        }
         Toast.makeText(this, "completed", Toast.LENGTH_SHORT).show(); //This is for all events so just check for rotation complete
     }
 
